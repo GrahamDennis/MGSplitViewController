5acc87133a17cb826d75bf8f97fea4a07bcd4f09
Graham Dennis <graham.dennis@gmail.com>
2012-06-16 18:21:39 +1000

    Cast the return type to UIViewController avoid compile warnings.

--- Classes/MGSplitViewController.m
+++ Classes/MGSplitViewController.m
@@ -1009,7 +1009,7 @@ if (self.view.window != nil) {
 	if (_viewControllers && [_viewControllers count] > 0) {
 		NSObject *controller = [_viewControllers objectAtIndex:0];
 		if ([controller isKindOfClass:[UIViewController class]]) {
-			return [[controller retain] autorelease];
+			return (UIViewController *)[[controller retain] autorelease];
 		}
 	}
 	
@@ -1051,7 +1051,7 @@ if (self.view.window != nil) {
 	if (_viewControllers && [_viewControllers count] > 1) {
 		NSObject *controller = [_viewControllers objectAtIndex:1];
 		if ([controller isKindOfClass:[UIViewController class]]) {
-			return [[controller retain] autorelease];
+			return (UIViewController *)[[controller retain] autorelease];
 		}
 	}
 	
